781f1ed4dfeef726dbab0ee9eac52b9fe4270055
Souvik Sarkar <1622721@TCS.com>
2020-10-20 14:56:47 +0530

    fixing test case

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -4,6 +4,7 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  //const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/One Health/i);
   expect(linkElement).toBeInTheDocument();
 });
